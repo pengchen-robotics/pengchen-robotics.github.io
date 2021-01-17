3394fd945a936bab4a7c4ea1e2f4be6bbe1fa082
pengchen-robotics <caseypen2013@gmail.com>
2021-01-17 11:26:50 -0800

    first commit

---
+++ content/pages/about.md
@@ -0,0 +1,10 @@
+Title: All about me
+Date: 2017-11-26 10:01
+Modified: 2017-11-27 12:30
+Category: misc
+Tags: about-me
+Slug: My-first-post
+Authors: Chen Peng
+Summary: This is all about me!
+
+*Initial post* from my Pelican blog. **YAY!**
---
+++ pelicanconf.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'Peng Chen'
+SITENAME = "Peng Chen's Homepage"
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Los_Angeles'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+STATIC_PATHS = ['images']
+# Blogroll
+LINKS = (('Ph.D. of UC, Davis', 'https://caseypen.github.io'),)
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
